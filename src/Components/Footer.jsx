ba6b5c70680b72a72b3300c9ed16c8bacb9b4b76
ZubairSadaqat <zubairsadaqat714@gmail.com>
2024-04-21 05:39:48 +0500

    Updated the Footer code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,16 @@ import logo from "../assets/name.png";
 
 const Footer = () => {
   return (
-    <div className=" max-w-[1200px] sm:h-auto p-12 flex justify-between mx-auto">
+    <div className="  max-w-[1200px] sm:h-auto p-12 flex flex-col md:flex-row justify-between mx-auto">
       {/* <span className="primary-color"> Zubair</span> */}
-      <img className="" src={logo} width={150} height={150} alt="" />
-      <p className="text-gray-600 my-auto">
+      <img
+        className="mx-auto md:mx-0"
+        src={logo}
+        width={150}
+        height={150}
+        alt=""
+      />
+      <p className="text-gray-600 my-auto mx-auto md:mx-0">
         {" "}
         Zubairsadaqat714@gmail.com
         <br /> @React_Dev_2024
